a528f593748726cd58f11e4128c54eb1fb5372c3
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2020-11-24 15:36:13 +0530

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,26 @@
 import React from 'react'
-import { Button, Form, FormControl, Nav } from 'react-bootstrap'
-import { Link } from 'react-router'
+// import { Button, Form, FormControl, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand>GNKart</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link><Link to="/profile">
-                        Profile
-                    </Link></Nav.Link>
-                    <Nav.Link><Link to="/cart">
-                        Cart
-                    </Link></Nav.Link>
-                    <Nav.Link><Link to="/signup">
-                        Sign UP
-                    </Link></Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">Cart</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Sign Up</Link>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
